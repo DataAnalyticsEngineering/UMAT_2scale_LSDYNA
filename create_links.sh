8c75cb698cd91c67c5561b4f8323f00b8e882910
Shadi Alameddin <shadialameddin@gmail.com>
2022-02-17 13:40:42 +0100

    separate setup files to be called without arguments

--- create_links.sh
+++ create_links.sh
@@ -4,3 +4,6 @@ current_dir=$(pwd)
 parent_dir=$(dirname $(pwd))
 ln -sf $current_dir/setup_dyna.sh $parent_dir/setup_dyna.sh
 ln -sf $current_dir/set_env.sh $parent_dir/set_env.sh
+ln -sf $current_dir/setup_quick.sh $parent_dir/setup_quick.sh
+ln -sf $current_dir/setup_full.sh $parent_dir/setup_full.sh
+ln -sf $current_dir/setup_debug.sh $parent_dir/setup_debug.sh
---
+++ setup_debug.sh
@@ -0,0 +1,1 @@
+./setup_dyna.sh d
