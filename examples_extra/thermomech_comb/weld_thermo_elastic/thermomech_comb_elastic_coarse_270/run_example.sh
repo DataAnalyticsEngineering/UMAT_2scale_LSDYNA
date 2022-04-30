bfcae6529f4e9d0a5b481b442abcc3a65038424c
Shadi Alameddin <shadialameddin@gmail.com>
2022-04-30 09:12:40 +0200

    auto save images of QoI at the last timestep

--- examples_extra/thermomech_comb/weld_thermo_elastic/thermomech_comb_elastic_coarse_270/run_example.sh
+++ examples_extra/thermomech_comb/weld_thermo_elastic/thermomech_comb_elastic_coarse_270/run_example.sh
@@ -1,3 +1,5 @@
 # /usr/bin/time -v 
 
 rm -rf sim* && /usr/bin/time -v lsdyna i=input_file.k ncpu=20 jobid=sim
+
+lsprepost c=post_process.cfile -nographics
--- examples_extra/thermomech_comb/weld_thermo_elastic/thermomech_comb_elastic_fine_slow_cooling_umat/run_example.sh
+++ examples_extra/thermomech_comb/weld_thermo_elastic/thermomech_comb_elastic_fine_slow_cooling_umat/run_example.sh
@@ -1,3 +1,5 @@
 # /usr/bin/time -v 
 
 rm -rf sim* && /usr/bin/time -v lsdynaumat i=input_file.k ncpu=1 jobid=sim
+
+lsprepost c=post_process.cfile -nographics
